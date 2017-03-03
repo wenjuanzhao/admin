8e14f79c2bf7e3a338f2c374857c1ee275ec732f
wenjuanzhao <1051743154@qq.com>
2017-03-03 11:32:43 +0800

    完成左侧nav

--- js/config.router.js
+++ js/config.router.js
@@ -4,14 +4,30 @@
  * Config for the router
  */
 angular.module('app')
-  .config(
-    [          '$stateProvider', '$urlRouterProvider',
-      function ($stateProvider,   $urlRouterProvider) {
-          $urlRouterProvider.otherwise('/app');
-          $stateProvider.state('app',{
-              url:"/app",
-              templateUrl:'tpls/app.html'
-          })
-      }
-    ]
-  );
+    .run(
+        [          '$rootScope', '$state', '$stateParams',
+            function ($rootScope,   $state,   $stateParams) {
+                $rootScope.$state = $state;
+                $rootScope.$stateParams = $stateParams;
+            }
+        ]
+    )
+    .config(
+        [          '$stateProvider', '$urlRouterProvider',
+            function ($stateProvider,   $urlRouterProvider) {
+
+                $urlRouterProvider
+                    .otherwise('/app/dashboard-v1');
+                $stateProvider
+                    .state('app', {
+                        abstract: true,
+                        url: '/app',
+                        templateUrl: 'tpl/app.html'
+                    })
+                    .state('app.dashboard-v1', {
+                        url: '/dashboard-v1',
+                        templateUrl: 'tpl/app_dashboard_v1.html',
+
+                    })}
+        ]
+    );
---
+++ tpls/app_dashboard-v1.html
@@ -0,0 +1,1 @@
+<div>sasa</div>
